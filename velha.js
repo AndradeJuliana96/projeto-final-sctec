dde30571868f31278ec1103fe970aa083b1f50db
Juliana Andrade <andrade96.juliana@gmail.com>
2026-02-22 18:12:14 -0300

    Update velha.js

--- velha.js
+++ velha.js
@@ -6,9 +6,9 @@ function iniciarJogoDaVelha() {
     let jogoAtivo = true;
 
     const combinacoesVitoria = [
-        [0,1,2], [3,4,5], [6,7,8], // linhas
-        [0,3,6], [1,4,7], [2,5,8], // colunas
-        [0,4,8], [2,4,6]           // diagonais
+        [0,1,2], [3,4,5], [6,7,8], 
+        [0,3,6], [1,4,7], [2,5,8], 
+        [0,4,8], [2,4,6]           
     ];
 
     function verificarVencedor() {
@@ -48,4 +48,5 @@ function iniciarJogoDaVelha() {
         jogadorAtual = "X";
         jogoAtivo = true;
     });
+
 }
